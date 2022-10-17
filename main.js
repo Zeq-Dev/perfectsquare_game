f1eaa591ac99a04a6a7989cb53466dc7057a0f16
Zeq-Dev <115923493+Zeq-Dev@users.noreply.github.com>
2022-10-17 20:44:45 +0800

    Optimized
    Fixed scoring bug

--- main.js
+++ main.js
@@ -1,71 +1,65 @@
-var score = document.getElementById('score');
-var queues = 0;
-var perfectSquare = '#' + queues;
+var queues;
+var squared;
 
-function randomNum() {
-    queues = 0;
-    score.innerHTML = 0;
-    perfectSquare = Math.floor(Math.random() * 30);
-    document.getElementById("number").innerHTML = perfectSquare + '²';
-    document.getElementById("start-button").style.display = "none";
-    document.getElementById("submit-button").style.display = "block";
-    document.getElementById("answer").disabled = false;
-    document.getElementById("answer").focus();   
+const answer_form = document.getElementById('answer');
+const answer_parent = document.getElementById('answerParent');
+const number_queue = document.querySelector('#number');
+const start_button = document.getElementById('start-button');
+const submit_button = document.getElementById('submit-button')
+const score = document.querySelector('#score');
+
+function randomNumber() {
+    let newNumber = Math.floor(Math.random() * 30);
+    squared = newNumber;
+    return newNumber;
 }
 
-function submitBut () {
-    var answer = document.getElementById('answer').value;
-    document.getElementById('answer').value = '';
-    console.log('#'+queues)
+function start() {
+    queues = 0;
+    perfectSquare = randomNumber();
+    number_queue.innerHTML = perfectSquare + '²';
+    start_button.style.display = "none";
+    submit_button.style.display = "block";
+    answer_form.disabled = false;
+    answer_form.focus();
+}
+
+function checkAns() {
     if (queues == 9) {
-        document.getElementById("number").innerHTML = "Press Start!";
-        document.getElementById("start-button").style.display = "block";
-        document.getElementById("submit-button").style.display = "none";
-        document.getElementById("answer").disabled = true;
-    } else if (perfectSquare * perfectSquare == answer) {
-        score.innerHTML = parseInt(score.innerHTML) + 1;
-        perfectSquare = Math.floor(Math.random() * 30);
-        document.getElementById("number").innerHTML = perfectSquare + '²';
+        if (squared * squared == answer_form.value) {
+            score.innerHTML = Number(score.innerHTML) + 1;
+        }
+        number_queue.innerHTML = "Press Start!";
+        start_button.style.display = "block";
+        submit_button.style.display = "none";
+        answer_form.disabled = true;
+    } else if (squared * squared == answer_form.value) {
+        score.innerHTML = Number(score.innerHTML) + 1;
+        perfectSquare = randomNumber();
+        number_queue.innerHTML = perfectSquare + '²';
         queues += 1;
-    } else if (answer == '') {
+    } else if (answer_form.value == '') {
         return;
-    } else {
-        console.log(perfectSquare * perfectSquare);
-        console.log('INCORRECT');
-        perfectSquare = Math.floor(Math.random() * 30);
-        document.getElementById("number").innerHTML = perfectSquare + '²';
+    } else if (squared * squared != answer_form.value) {
+        console.log(squared * squared + 'CORRECT ANS,');
+        perfectSquare = randomNumber();
+        number_queue.innerHTML = perfectSquare + '²';
         queues += 1;
     }
 }
 
-function subAnw() {
-    document.addEventListener("keyup", function(event) {
-        if (event.code === 'Enter' || event.keyCode === 13) {
-            var answer = document.getElementById('answer').value;
-            document.getElementById('answer').value = '';
-            console.log('#'+queues)
-            if (queues == 9) {
-                document.getElementById("number").innerHTML = "Press Start!";
-                document.getElementById("start-button").style.display = "block";
-                document.getElementById("submit-button").style.display = "none";
-                document.getElementById("answer").disabled = true;
-                if (perfectSquare * perfectSquare == answer) {
-                    score.innerHTML = parseInt(score.innerHTML) + 1;
-                }
-            } else if (perfectSquare * perfectSquare == answer) {
-                score.innerHTML = parseInt(score.innerHTML) + 1;
-                perfectSquare = Math.floor(Math.random() * 30);
-                document.getElementById("number").innerHTML = perfectSquare + '²';
-                queues += 1;
-            } else if (answer == '') {
-                return;
-            } else if (answer != '') {
-                console.log(perfectSquare * perfectSquare);
-                console.log('INCORRECT');
-                perfectSquare = Math.floor(Math.random() * 30);
-                document.getElementById("number").innerHTML = perfectSquare + '²';
-                queues += 1;
-            }
+function enterSub() {
+    answer_parent.addEventListener("submit", e => {
+        if (answer_form.value === '') {
+            e.preventDefault();
+        } else {
+            checkAns();
+            answer_form.value = '';
         }
     })
+}
+
+function buttonSub() {
+    checkAns();
+    answer_form.value = '';
 }
